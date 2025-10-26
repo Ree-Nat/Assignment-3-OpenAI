1a2f436b64398728130d2c830197fb29d775d60e
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2025-10-26 13:48:02 -0700

    add maven dependencies and readme files

--- demo/src/main/java/org/example/demo/Launcher.java
+++ demo/src/main/java/org/example/demo/Launcher.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+
+
         Application.launch(HelloApplication.class, args);
+
+
     }
 }
